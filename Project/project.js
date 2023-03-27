04dedd818a30778409030af9c3a112d75b484584
Valerie Wireko Osei <valeriew@SML391TO.local>
2023-03-27 19:44:10 -0400

    day 8 work

--- Project/project.js
+++ Project/project.js
@@ -1,13 +1,14 @@
 // Our variables
 var submitButton = document.querySelector('.button');
-var task = document.getElementById('name');
+var task = document.getElementById('task');
 var description = document.getElementById('description');
 var date = document.getElementById('date');
+var taskContainer = document.getElementById('task-container');
 
 // Creating our Task Object
 class Task {
-    constructor(name, description, date) {
-        this.name = name;
+    constructor(task, description, date) {
+        this.task = task;
         this.description = description;
         this.date = date;
     }
@@ -20,7 +21,24 @@ function submitTask(event) {
     event.preventDefault();
     const newTask = new Task(task.value, description.value, date.value);
 
+    var singleTask = document.createElement('div');
+    var taskTitle = document.createElement('h3');
+    var taskDescription = document.createElement('p');
+    var taskDate = document.createElement('span');
+
+    singleTask.classList.add('single-task');
+
+    taskTitle.innerText = newTask.task;
+    taskDescription.innerText = newTask.description;
+    taskDate.innerText = newTask.date;
+
+    singleTask.appendChild(taskTitle);
+    singleTask.appendChild(taskDescription);
+    singleTask.appendChild(taskDate);
+
     console.log(newTask);
+
+    taskContainer.appendChild(singleTask);
 }
 
 submitButton.addEventListener('click', submitTask);
